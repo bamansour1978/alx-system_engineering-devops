bc05f475835ec41cba4f4cc745057fc4036212ff
bamansour1978 <karatman@gmail.com>
2023-10-24 00:40:03 +0100

    3. Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Using what you did in the task #0, extend your Python
+script to export data in the JSON format.
+"""
+
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            us.get("id"): [{
+                "task": ti.get("title"),
+                "completed": ti.get("completed"),
+                "username": us.get("username")
+            } for ti in requests.get(url + "todos",
+                                     params={"userId": us.get("id")}).json()]
+            for us in users}, jsonfile)
