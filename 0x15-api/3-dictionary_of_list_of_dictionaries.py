2fb56f58d7ce29b6dd8410170aa9050ce7d30103
bamansour1978 <karatman@gmail.com>
2023-10-24 00:51:21 +0100

    Update

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,19 +1,39 @@
-
 #!/usr/bin/python3
-"""---"""
+""" Task 3"""
 import json
 import requests
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
 
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+def main():
+    id = 1
+    data_dict = {}
+
+    while True:
+        url = "https://jsonplaceholder.typicode.com/"
+        users = f"users?id={id}"
+        todos = f"todos?userId={id}"
+        userData = requests.get(f"{url}{users}").json()
+
+        if not userData:
+            break
+
+        userName = userData[0].get("username")
+        todosData = requests.get(f"{url}{todos}").json()
+
+        data_dict[id] = [
+            {
+                "username": userName,
+                "task": task.get("title"),
+                "completed": task.get("completed")
+            }
+            for task in todosData
+        ]
+
+        id += 1
+
+    with open("todo_all_employees.json", "w") as f:
+        json.dump(data_dict, f)
+
+
+if __name__ == "__main__":
+    main()
