82e2508b056b34fe0c1036c29b4b473a6d202266
bamansour1978 <karatman@gmail.com>
2023-11-08 23:04:52 +0100

    Update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+
+exec { 'fix wordpress':
+  command     => "/bin/sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
 }
